2d4eebfe084677fcac5e3a04baf0e03a2faca289
Daffa Destrata <daffa123destrata@gmail.com>
2020-05-16 06:03:36 +0700

    Menambahkan file covid 19 ino

---
+++ covid_19_tracker.ino
@@ -0,0 +1,43 @@
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid = "b4871e";
+const char* pass = "277123586";
+
+HTTPClient http;
+
+const size_t capacity = JSON_ARRAY_SIZE(2) + JSON_OBJECT_SIZE(3);
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("CONNECT BOSSS");
+}
+
+void loop () {
+  http.begin("http://kawalcorona.com");
+  int httpCode = http.GET();
+
+  if (httpCode > 0)
+  {
+    char json[];
+    const char* payload = http.getString();
+    StaticJsonDocument<256> doc;
+    payload.toCharArray(doc, json);
+    deserializeJson(doc,json);
+    const char* tanggal = doc["update"];
+    int umur = doc["sembuh"];
+
+    Serial.println("tanggal",tanggal);
+    
+  }
+  
+  http.end();
+  delay(2000);
+}
