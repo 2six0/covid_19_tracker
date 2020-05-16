387491ea273b782f1735131cc3409bc2741d3bf3
Daffa Destrata <daffa123destrata@gmail.com>
2020-05-16 19:21:09 +0700

    edited dari vscode

--- covid_19_tracker.ino
+++ covid_19_tracker.ino
@@ -8,39 +8,24 @@ const char* pass = "277123586";
 
 HTTPClient http;
 
-const size_t capacity = JSON_ARRAY_SIZE(2) + JSON_OBJECT_SIZE(3);
-
 void setup() {
   Serial.begin(9600);
   WiFi.begin(ssid, pass);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println(".");
+    delay(200);
   }
-  Serial.println("CONNECT BOSSS");
 }
 
 void loop () {
-  http.begin("https://api.kawalcorona.com/indonesia/provinsi/");
-  int httpCode = http.GET();
-
-  if (httpCode > 0)
-  {
-    char json[500];
-    const char* payload = http.getString();
-    StaticJsonDocument<256> doc;
-    payload.toCharArray(doc, json);
-    deserializeJson(doc,json);
-    const char* tanggal = doc["update"];
-    int umur = doc["sembuh"];
-
-    Serial.println("tanggal",tanggal);
-    
+  if (WiFi.status() == WL_CONNECTED) {
+    http.begin("https://api.kawalcorona.com/indonesia/");
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+    http.end();
   }
-  
-  http.end();
   delay(2000);
-
- // test_led(2,200);
 }
